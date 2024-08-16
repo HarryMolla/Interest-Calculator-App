aac534d9719672f9fddb88946c0237c61f0be745
unknown <hmiol5577@gmail.com>
2024-08-16 09:53:42 +0300

    Color update for Color mode and resat update

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
@@ -27,10 +28,10 @@ class _harryCurrencyState extends State<harryCurrency> {
   String? principalError;
   String? termError;
   String? RoiError;
-  bool _isWhite=true;
-  void _bagroundColorToggle(){
+  bool _isWhite = true;
+  void _bagroundColorToggle() {
     setState(() {
-      _isWhite=!_isWhite;
+      _isWhite = !_isWhite;
     });
   }
 
@@ -73,39 +74,42 @@ class _harryCurrencyState extends State<harryCurrency> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: _isWhite? const Color.fromARGB(255, 240, 240, 240):Colors.black,
+      backgroundColor:
+          _isWhite ? const Color.fromARGB(255, 240, 240, 240) : Colors.black,
       appBar: AppBar(
-        backgroundColor: _isWhite? const Color.fromARGB(255, 240, 240, 240):Colors.black,
+        backgroundColor:
+            _isWhite ? const Color.fromARGB(255, 240, 240, 240) : Colors.black,
         actions: [
           IconButton(
-            onPressed: _bagroundColorToggle, 
-            icon: Icon(
-              _isWhite? Icons.sunny:Icons.dark_mode
-              ),
-              color: _isWhite? Colors.black:Colors.white,
-              ),
-              Icon(Icons.abc, color: Color.fromRGBO(0, 0, 0, 0),)
-          ],),
-      body: SingleChildScrollView(child: 
-      Center(
+            onPressed: _bagroundColorToggle,
+            icon: Icon(_isWhite ? Icons.sunny : Icons.dark_mode),
+            color: _isWhite ? Colors.black : Colors.white,
+          ),
+          Icon(
+            Icons.abc,
+            color: Color.fromRGBO(0, 0, 0, 0),
+          )
+        ],
+      ),
+      body: SingleChildScrollView(
+          child: Center(
         child: Form(
           key: _formKey,
           child: Padding(
               padding: EdgeInsets.all(10),
               child: Column(
                 children: [
-                  
                   Stack(
                     alignment: Alignment.bottomCenter,
                     children: [
                       Image.asset('lib/Img/interst.png',
                           width: 200, height: 200),
-                      const Text(
+                       Text(
                         'Interest Calculator',
                         style: TextStyle(
                           fontWeight: FontWeight.bold,
                           fontSize: 22,
-                          color: Color.fromRGBO(2, 55, 55, 1),
+                          color:_isWhite? Color.fromRGBO(2, 55, 55, 1):Color.fromRGBO(3, 99, 99, 1),
                         ),
                       ),
                     ],
@@ -113,88 +117,156 @@ class _harryCurrencyState extends State<harryCurrency> {
                   SizedBox(
                     height: 40,
                   ),
-                 SizedBox(
-                  width: 480,
-                  child:  TextFormField(
-                    onChanged: validatePrincipal,
-                    controller: principalController,
-                    keyboardType: TextInputType.number,
-                    decoration: InputDecoration(
-                      label: Text('Principal'),
-                      errorText: principalError,
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
+                  SizedBox(
+                    width: 480,
+                    child: TextFormField(
+                      style: TextStyle(
+                          color: _isWhite ? Colors.black : Colors.white),
+                      onChanged: validatePrincipal,
+                      controller: principalController,
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        focusedBorder: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(10),
+                                      borderSide: BorderSide(
+                                          color: Colors.green, width: 1.5)),
+                        label: Text('Principal'),
+                        errorText: principalError,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
                       ),
                     ),
                   ),
-                 ),
                   SizedBox(
                     height: 10,
                   ),
                   SizedBox(
                     width: 480,
                     child: TextFormField(
-                    onChanged: validateRoi,
-                    controller: roiController,
-                    decoration: InputDecoration(
-                        errorText: RoiError,
-                        label: Text('Rate of Interest'),
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10))),
-                  ),
+                      style: TextStyle(
+                          color: _isWhite ? Colors.black : Colors.white),
+                      onChanged: validateRoi,
+                      controller: roiController,
+                      decoration: InputDecoration(
+                        focusedBorder: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(10),
+                                      borderSide: BorderSide(
+                                          color: Colors.green, width: 1.5)),
+                          errorText: RoiError,
+                          label: Text('Rate of Interest'),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10))),
+                    ),
                   ),
                   SizedBox(
                     height: 10,
                   ),
                   Center(
-                    
-                    child: 
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      SizedBox(
-                          width: 235,
-                          child: TextFormField(
-                            onChanged: validateTerm,
-                            controller: termController,
-                            decoration: InputDecoration(
-                                errorText: termError,
-                                label: Text('Term'),
-                                border: OutlineInputBorder(
-                                    borderRadius: BorderRadius.circular(10))),
-                          )),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      SizedBox(
-                          width: 235,
-                          height: 55,
-                          child: Container(
-                              decoration: BoxDecoration(
-                                  color: Color.fromARGB(255, 235, 235, 235),
-                                  borderRadius: BorderRadius.circular(10)),
-                              child: DropdownButtonHideUnderline(
-                                child: DropdownButton(
-                                  borderRadius: BorderRadius.circular(10),
-                                  hint: Text('Select Curruncy'),
-                                  value: smallValue,
-                                  padding: EdgeInsets.only(left: 40, right: 45),
-                                  items: bigValue.map((String newValue) {
-                                    return DropdownMenuItem<String>(
-                                      value: newValue,
-                                      child: Center(child: Text(newValue)),
-                                    );
-                                  }).toList(),
-                                  onChanged: <String>(someValue) {
-                                    setState(() {
-                                      smallValue = someValue;
-                                    });
-                                  },
-                                ),
-                              )))
-                    ],
-                  ),),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                            width: 235,
+                            child: TextFormField(
+                              style: TextStyle(
+                                  color:
+                                      _isWhite ? Colors.black : Colors.white),
+                              onChanged: validateTerm,
+                              controller: termController,
+                              decoration: InputDecoration(
+                                  label: Text(
+                                    'Term',
+                                    style: TextStyle(color: Colors.grey),
+                                  ),
+                                  focusedBorder: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(10),
+                                      borderSide: BorderSide(
+                                          color: Colors.green, width: 1.5)),
+                                  errorText: termError,
+                                  border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(10))),
+                            )),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        SizedBox(
+                            width: 235,
+                            height: 55,
+                            child: Container(
+                                decoration: BoxDecoration(
+                                    color: _isWhite
+                                        ? Color.fromARGB(255, 194, 194, 194)
+                                        : Color.fromARGB(255, 27, 27, 27),
+                                    borderRadius: BorderRadius.circular(10)),
+                                child: DropdownButtonHideUnderline(
+                                  child: DropdownButton(
+                                    dropdownColor: _isWhite
+                                        ? Colors.white
+                                        : Color.fromARGB(255, 31, 31, 31),
+                                    borderRadius: BorderRadius.circular(10),
+                                    hint: Text(
+                                      'Select Curruncy',
+                                      style: TextStyle(
+                                          color: _isWhite
+                                              ? const Color.fromARGB(
+                                                  255, 24, 24, 24)
+                                              : const Color.fromARGB(
+                                                  255, 223, 223, 223)),
+                                    ),
+                                    value: smallValue,
+                                    padding:
+                                        EdgeInsets.only(left: 40, right: 45),
+                                    items: bigValue.map((String newValue) {
+                                      return DropdownMenuItem<String>(
+                                        value: newValue,
+                                        child: Column(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          children: [
+                                            if (bigValue.indexOf(newValue) > 0)
+                                              if (bigValue.indexOf(newValue) <
+                                                  3)
+                                                Divider(
+                                                    height: 0.01,
+                                                    color: _isWhite
+                                                        ? Color.fromARGB(
+                                                            255, 241, 241, 241)
+                                                        : Color.fromARGB(
+                                                            255, 12, 12, 12)),
+                                            Padding(
+                                              padding:
+                                                  const EdgeInsets.symmetric(
+                                                      vertical: 11.0),
+                                              child: Center(
+                                                  child: Text(
+                                                newValue,
+                                                style: TextStyle(
+                                                    color: _isWhite
+                                                        ? const Color.fromARGB(
+                                                            255, 24, 24, 24)
+                                                        : const Color.fromARGB(
+                                                            255,
+                                                            223,
+                                                            223,
+                                                            223)),
+                                              )),
+                                            ),
+                                          ],
+                                        ),
+                                      );
+                                    }).toList(),
+                                    onChanged: <String>(someValue) {
+                                      setState(() {
+                                        smallValue = someValue;
+                                      });
+                                    },
+                                  ),
+                                )))
+                      ],
+                    ),
+                  ),
                   SizedBox(
                     height: 10,
                   ),
@@ -234,6 +306,9 @@ class _harryCurrencyState extends State<harryCurrency> {
                         height: 55,
                         child: ElevatedButton(
                             style: ElevatedButton.styleFrom(
+                                backgroundColor: _isWhite
+                                    ? Color.fromARGB(255, 253, 213, 210)
+                                    : const Color.fromARGB(255, 70, 19, 16),
                                 side: BorderSide(width: 1, color: Colors.red),
                                 shape: RoundedRectangleBorder(
                                     borderRadius: BorderRadius.circular(10))),
@@ -252,8 +327,16 @@ class _harryCurrencyState extends State<harryCurrency> {
                   SizedBox(
                     height: 15,
                   ),
-                  Text('Result:'),
-                  Text(theDisplay)
+                  Text(
+                    'Result:',
+                    style: TextStyle(
+                        color: _isWhite ? Colors.black : Colors.white),
+                  ),
+                  Text(
+                    theDisplay,
+                    style: TextStyle(
+                        color: _isWhite ? Colors.black : Colors.white),
+                  )
                 ],
               )),
         ),
@@ -277,5 +360,8 @@ class _harryCurrencyState extends State<harryCurrency> {
     termController.text = '';
     theDisplay = '';
     smallValue = bigValue[0];
+    principalError = null;
+    termError = null;
+    RoiError = null;
   }
 }
