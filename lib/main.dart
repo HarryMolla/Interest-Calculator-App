4d467242828320bdb433467be493e6b4a2e7c272
unknown <hmiol5577@gmail.com>
2024-08-18 12:18:26 +0300

    I make it responsive for d/t devices

--- lib/main.dart
+++ lib/main.dart
@@ -82,7 +82,13 @@ class _harryCurrencyState extends State<harryCurrency> {
         actions: [
           IconButton(
             onPressed: _bagroundColorToggle,
-            icon: Icon(_isWhite ? Icons.sunny : Icons.dark_mode),
+            icon: AnimatedSwitcher(
+              duration: Duration(milliseconds: 900),
+              child: Icon(
+                _isWhite ? Icons.sunny : Icons.dark_mode,
+                size: 24,
+              ),
+            ), //Icon(_isWhite ? Icons.sunny : Icons.dark_mode),
             color: _isWhite ? Colors.black : Colors.white,
           ),
           Icon(
@@ -104,12 +110,14 @@ class _harryCurrencyState extends State<harryCurrency> {
                     children: [
                       Image.asset('lib/Img/interst.png',
                           width: 200, height: 200),
-                       Text(
+                      Text(
                         'Interest Calculator',
                         style: TextStyle(
                           fontWeight: FontWeight.bold,
                           fontSize: 22,
-                          color:_isWhite? Color.fromRGBO(2, 55, 55, 1):Color.fromRGBO(3, 99, 99, 1),
+                          color: _isWhite
+                              ? Color.fromRGBO(2, 55, 55, 1)
+                              : Color.fromRGBO(3, 99, 99, 1),
                         ),
                       ),
                     ],
@@ -117,84 +125,84 @@ class _harryCurrencyState extends State<harryCurrency> {
                   SizedBox(
                     height: 40,
                   ),
-                  SizedBox(
-                    width: 480,
-                    child: TextFormField(
-                      style: TextStyle(
-                          color: _isWhite ? Colors.black : Colors.white),
-                      onChanged: validatePrincipal,
-                      controller: principalController,
-                      keyboardType: TextInputType.number,
-                      decoration: InputDecoration(
-                        focusedBorder: OutlineInputBorder(
-                                      borderRadius: BorderRadius.circular(10),
-                                      borderSide: BorderSide(
-                                          color: Colors.green, width: 1.5)),
-                        label: Text('Principal'),
-                        errorText: principalError,
-                        border: OutlineInputBorder(
+                  TextFormField(
+                    style: TextStyle(
+                        color: _isWhite ? Colors.black : Colors.white),
+                    onChanged: validatePrincipal,
+                    controller: principalController,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      focusedBorder: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(10),
-                        ),
+                          borderSide:
+                              BorderSide(color: Colors.green, width: 1.5)),
+                      label: Text(
+                        'Principal',
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                      errorText: principalError,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
                       ),
                     ),
                   ),
                   SizedBox(
                     height: 10,
                   ),
-                  SizedBox(
-                    width: 480,
-                    child: TextFormField(
-                      style: TextStyle(
-                          color: _isWhite ? Colors.black : Colors.white),
-                      onChanged: validateRoi,
-                      controller: roiController,
-                      decoration: InputDecoration(
+                  TextFormField(
+                    style: TextStyle(
+                        color: _isWhite ? Colors.black : Colors.white),
+                    onChanged: validateRoi,
+                    controller: roiController,
+                    decoration: InputDecoration(
                         focusedBorder: OutlineInputBorder(
-                                      borderRadius: BorderRadius.circular(10),
-                                      borderSide: BorderSide(
-                                          color: Colors.green, width: 1.5)),
-                          errorText: RoiError,
-                          label: Text('Rate of Interest'),
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10))),
-                    ),
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide:
+                                BorderSide(color: Colors.green, width: 1.5)),
+                        errorText: RoiError,
+                        label: Text(
+                          'Rate of Interest',
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10))),
                   ),
                   SizedBox(
                     height: 10,
                   ),
                   Center(
                     child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
-                        SizedBox(
-                            width: 235,
-                            child: TextFormField(
-                              style: TextStyle(
-                                  color:
-                                      _isWhite ? Colors.black : Colors.white),
-                              onChanged: validateTerm,
-                              controller: termController,
-                              decoration: InputDecoration(
-                                  label: Text(
-                                    'Term',
-                                    style: TextStyle(color: Colors.grey),
-                                  ),
-                                  focusedBorder: OutlineInputBorder(
-                                      borderRadius: BorderRadius.circular(10),
-                                      borderSide: BorderSide(
-                                          color: Colors.green, width: 1.5)),
-                                  errorText: termError,
-                                  border: OutlineInputBorder(
-                                      borderRadius: BorderRadius.circular(10))),
-                            )),
+                        Expanded(
+                          flex: 1,
+                          child: TextFormField(
+                            style: TextStyle(
+                                color: _isWhite ? Colors.black : Colors.white),
+                            onChanged: validateTerm,
+                            controller: termController,
+                            decoration: InputDecoration(
+                                label: Text(
+                                  'Term ',
+                                  style: TextStyle(
+                                      color: Colors.grey,
+                                      overflow: TextOverflow.ellipsis),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                    borderSide: BorderSide(
+                                        color: Colors.green, width: 1.5)),
+                                errorText: termError,
+                                border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10))),
+                          ),
+                        ),
                         SizedBox(
                           width: 10,
                         ),
-                        SizedBox(
-                            width: 235,
-                            height: 55,
+                        Expanded(
+                            flex: 1,
                             child: Container(
+                                height: 55,
                                 decoration: BoxDecoration(
                                     color: _isWhite
                                         ? Color.fromARGB(255, 194, 194, 194)
@@ -202,6 +210,7 @@ class _harryCurrencyState extends State<harryCurrency> {
                                     borderRadius: BorderRadius.circular(10)),
                                 child: DropdownButtonHideUnderline(
                                   child: DropdownButton(
+                                    isExpanded: true,
                                     dropdownColor: _isWhite
                                         ? Colors.white
                                         : Color.fromARGB(255, 31, 31, 31),
@@ -209,6 +218,7 @@ class _harryCurrencyState extends State<harryCurrency> {
                                     hint: Text(
                                       'Select Curruncy',
                                       style: TextStyle(
+                                          overflow: TextOverflow.ellipsis,
                                           color: _isWhite
                                               ? const Color.fromARGB(
                                                   255, 24, 24, 24)
@@ -220,42 +230,14 @@ class _harryCurrencyState extends State<harryCurrency> {
                                         EdgeInsets.only(left: 40, right: 45),
                                     items: bigValue.map((String newValue) {
                                       return DropdownMenuItem<String>(
-                                        value: newValue,
-                                        child: Column(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.center,
-                                          children: [
-                                            if (bigValue.indexOf(newValue) > 0)
-                                              if (bigValue.indexOf(newValue) <
-                                                  3)
-                                                Divider(
-                                                    height: 0.01,
-                                                    color: _isWhite
-                                                        ? Color.fromARGB(
-                                                            255, 241, 241, 241)
-                                                        : Color.fromARGB(
-                                                            255, 12, 12, 12)),
-                                            Padding(
-                                              padding:
-                                                  const EdgeInsets.symmetric(
-                                                      vertical: 11.0),
-                                              child: Center(
-                                                  child: Text(
-                                                newValue,
-                                                style: TextStyle(
-                                                    color: _isWhite
-                                                        ? const Color.fromARGB(
-                                                            255, 24, 24, 24)
-                                                        : const Color.fromARGB(
-                                                            255,
-                                                            223,
-                                                            223,
-                                                            223)),
-                                              )),
-                                            ),
-                                          ],
-                                        ),
-                                      );
+                                          value: newValue,
+                                          child: Text(
+                                            newValue,
+                                            style: TextStyle(
+                                                color: _isWhite
+                                                    ? Colors.black
+                                                    : Colors.white),
+                                          ));
                                     }).toList(),
                                     onChanged: <String>(someValue) {
                                       setState(() {
@@ -273,55 +255,64 @@ class _harryCurrencyState extends State<harryCurrency> {
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      SizedBox(
-                        width: 235,
-                        height: 55,
-                        child: ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                                backgroundColor:
-                                    const Color.fromARGB(255, 5, 126, 9),
-                                shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10))),
-                            clipBehavior: Clip.hardEdge,
-                            onPressed: () {
-                              validatePrincipal(principalController.text);
-                              validateRoi(roiController.text);
-                              validateTerm(termController.text);
+                      Expanded(
+                          flex: 1,
+                          child: Container(
+                            height: 55,
+                            child: ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                    backgroundColor:
+                                        const Color.fromARGB(255, 5, 126, 9),
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(10))),
+                                clipBehavior: Clip.hardEdge,
+                                onPressed: () {
+                                  validatePrincipal(principalController.text);
+                                  validateRoi(roiController.text);
+                                  validateTerm(termController.text);
 
-                              if (_formKey.currentState != null &&
-                                  _formKey.currentState!.validate()) {
-                                this.theDisplay = caluculatTotalReturn();
-                              }
-                            },
-                            child: Text(
-                              'Calculate',
-                              style: TextStyle(color: Colors.white),
-                            )),
-                      ),
+                                  if (_formKey.currentState != null &&
+                                      _formKey.currentState!.validate()) {
+                                    this.theDisplay = caluculatTotalReturn();
+                                  }
+                                },
+                                child: Text(
+                                  'Calculate',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      overflow: TextOverflow.ellipsis),
+                                )),
+                          )),
                       SizedBox(
                         width: 10,
                       ),
-                      SizedBox(
-                        width: 235,
-                        height: 55,
-                        child: ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                                backgroundColor: _isWhite
-                                    ? Color.fromARGB(255, 253, 213, 210)
-                                    : const Color.fromARGB(255, 70, 19, 16),
-                                side: BorderSide(width: 1, color: Colors.red),
-                                shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10))),
-                            onPressed: () {
-                              setState(() {
-                                resatResulut();
-                              });
-                            },
-                            child: Text(
-                              'Reset',
-                              style: TextStyle(color: Colors.red),
-                            )),
-                      ),
+                      Expanded(
+                          flex: 1,
+                          child: Container(
+                            height: 55,
+                            child: ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                    backgroundColor: _isWhite
+                                        ? Color.fromARGB(255, 253, 213, 210)
+                                        : const Color.fromARGB(255, 70, 19, 16),
+                                    side:
+                                        BorderSide(width: 1, color: Colors.red),
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(10))),
+                                onPressed: () {
+                                  setState(() {
+                                    resatResulut();
+                                  });
+                                },
+                                child: Text(
+                                  'Reset',
+                                  style: TextStyle(
+                                      color: Colors.red,
+                                      overflow: TextOverflow.ellipsis),
+                                )),
+                          )),
                     ],
                   ),
                   SizedBox(
